9086891f1e8736f85b30cdf7f2adf9e6a4291215
NjohPrince <jufredprince@gmail.com>
2020-11-30 09:33:48 +0100

    created and about section

--- mywebsite/src/components/row/content.jsx
+++ mywebsite/src/components/row/content.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
+const ContainerImg = styled.img`
+  width: 22em;
+  height: 18em;
+`;
+
+const ImgDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+  align-items: center;
+`;
+
+const Name = styled.h2`
+  color: #000;
+  font-size: 27px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const Description = styled.h6`
+  color: gray;
+  font-size: 20px;
+  font-weight: bold
+`;
 
 export function SingleRow(props) {
-  return ;
+  const { name, descript, imgURL } = props;
+  return (
+    <Container>
+      <ImgDescription>
+        <Name>{name}</Name>
+        <Description>{descript}</Description>
+      </ImgDescription>
+      <ContainerImg src={imgURL} />
+    </Container>
+  );
 }
--- mywebsite/src/containers/aboutSection.jsx
+++ mywebsite/src/containers/aboutSection.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Element } from 'react-scroll'
-import styled from 'styled-components';
-import { CaptionText } from '../components/textCaption/caption';
+import React from "react";
+import { Element } from "react-scroll";
+import styled from "styled-components";
+import { SingleRow } from "../components/row/content";
+import { CaptionText } from "../components/textCaption/caption";
+import rowImage from "../assets/images/cont.jfif";
 
-const Container = styled( Element )`
+const Container = styled(Element)`
   width: 100%;
   min-height: 500px;
   display: flex;
@@ -11,8 +13,15 @@ const Container = styled( Element )`
   align-items: center;
 `;
 
-export function AboutSection(props){
-    return <Container name="AboutSection">
-        <CaptionText>About Me</CaptionText>
-    </Container>;
+export function AboutSection(props) {
+  return (
+    <Container name="AboutSection">
+      <CaptionText>About Me</CaptionText>
+      <SingleRow
+        name="Learn and Grow Together"
+        descript="Just a CodeLover, who helps others acquire such skills too"
+        imgURL={rowImage}
+      />
+    </Container>
+  );
 }
